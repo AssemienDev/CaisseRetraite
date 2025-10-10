b674e80871a39e6990cc868b6bf4006442b16ced
AssemienDev <139700557+AssemienDev@users.noreply.github.com>
2025-10-10 00:03:53 +0000

    feat: Mise en place du déploiement CI/CD 20

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,18 @@
 echo "Applying database migrations..."
 python manage.py migrate
 
+# Créer le superutilisateur si il n'existe pas
+echo "Creating superuser if not exists..."
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="crrae").exists():
+    User.objects.create_superuser("crrae", "crrae@gmail.com", "mdp12354@A")
+    print("Superuser created")
+else:
+    print("Superuser already exists")
+END
+
 # Collecter les fichiers statiques
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
